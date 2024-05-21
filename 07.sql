8ddf6ccb42f0f99151e1283cc04d6e1937b1a7c6
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-05-21 21:25:41 +0300

    21.05.2024

--- 07.sql
+++ 07.sql
@@ -14,8 +14,37 @@ WHERE
 	SUBSTRING(title, 1, 3) = 'The'
 ORDER BY id;
 --3
-
+SELECT 
+	id,
+	(side * height) / 2 AS area
+FROM 
+	triangles;
 --4
+SELECT 
+	title,
+	TRUNC(cost, 3) AS modified_price
+FROM 
+	books
+ORDER BY id;
 --5
+SELECT 
+	first_name,
+	last_name,
+	EXTRACT(year FROM born) AS year
+FROM 
+	authors
+;
 --6
+SELECT 
+	last_name AS "Last Name",
+	TO_CHAR(born, 'DD (Dy) Mon YYYY') AS "Date of Birth"
+FROM 
+	authors
+;
 --7
+SELECT 
+	title
+FROM 
+	books
+WHERE title LIKE 'Harry Potter%'
+Order BY id;
