225d84658492eae10d6f6e23b9fba88a268b9c42
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-05-25 10:57:09 +0300

    25.05.2024

---
+++ 09.sql
@@ -0,0 +1,79 @@
+--1
+SELECT 
+	department_id, 
+ 	COUNT(department_id) AS employee_count
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--2
+SELECT 
+	department_id, 
+ 	COUNT(salary) AS employee_count
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--3
+SELECT 
+	department_id, 
+ 	SUM(salary) AS total_salaries
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--4
+SELECT 
+	department_id, 
+ 	MAX(salary) AS max_salary
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--5
+SELECT 
+	department_id, 
+ 	MIN(salary) AS min_salary
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--6
+SELECT 
+	department_id, 
+ 	AVG(salary) AS avg_salary
+FROM employees
+GROUP BY 
+	department_id
+ORDER BY 
+	department_id;
+--7
+SELECT 
+	department_id, 
+ 	SUM(salary) AS "Total Salary"
+FROM employees
+GROUP BY 
+	department_id
+HAVING SUM(salary) < 4200
+ORDER BY 
+	department_id;
+--8
+SELECT 
+	id,
+	first_name,
+	last_name,
+	ROUND(salary, 2) as salary,
+	department_id,
+	CASE
+		WHEN department_id = 1 THEN 'Management'
+		WHEN department_id = 2 THEN 'Kitchen Staff'
+		WHEN department_id = 3 THEN 'Service Staff'
+	ELSE 'Other'
+	END AS department_name
+FROM employees
+;
