113191faa1c425890911d150549ecc4650870f7f
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-05-29 20:49:30 +0300

    29.05.2024

---
+++ 11.sql
@@ -0,0 +1,18 @@
+--1
+CREATE TABLE mountains(
+	id serial PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE peaks(
+	id serial PRIMARY KEY,
+	name VARCHAR(50),
+	mountain_id INT,
+	CONSTRAINT fk_peaks_mountains
+		FOREIGN KEY (mountain_id)
+		REFERENCES mountains(id)
+);
+--2
+
+--3
+--4
