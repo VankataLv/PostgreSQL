47f08d314ac638c1a8ef9191d4366146fb31d788
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-06-01 10:15:41 +0300

    01.06.2024

--- 12.sql
+++ 12.sql
@@ -201,11 +201,125 @@ VALUES
 ;
 
 --10
+CREATE TABLE item_types (
+	id SERIAL PRIMARY KEY,
+	item_type_name VARCHAR(50)
+);
+CREATE TABLE items (
+	id SERIAL PRIMARY KEY,
+	item_name VARCHAR(100),
+	item_type_id INT,
 
---
---
---
---
---
---
---
+	CONSTRAINT fk_items_item_types
+	FOREIGN KEY (item_type_id)
+	REFERENCES item_types(id)
+);
+CREATE TABLE cities (
+	id SERIAL PRIMARY KEY,
+	city_name VARCHAR(50)
+);
+CREATE TABLE customers (
+	id SERIAL PRIMARY KEY,
+	customer_name VARCHAR(100),
+	birthday DATE,
+	city_id INT,
+
+	CONSTRAINT fk_customers_cities
+	FOREIGN KEY (city_id)
+	REFERENCES cities(id)
+);
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	customer_id INT,
+
+	CONSTRAINT fk_orders_customers
+	FOREIGN KEY (customer_id)
+	REFERENCES customers(id)
+);
+CREATE TABLE order_items (
+	order_id INT,
+	item_id INT,
+	
+	CONSTRAINT pk_order_items
+	PRIMARY KEY (order_id, item_id),
+
+	CONSTRAINT fk_order_items_orders
+	FOREIGN KEY (order_id)
+	REFERENCES orders(id),
+
+	CONSTRAINT fk_order_items_items
+	FOREIGN KEY (item_id)
+	REFERENCES items(id)
+);
+--11
+ALTER TABLE countries
+	
+ADD CONSTRAINT fk_countries_continents
+FOREIGN KEY (continent_code)
+REFERENCES continents(continent_code)
+	ON DELETE CASCADE,
+
+ADD CONSTRAINT fk_countries_currencies
+FOREIGN KEY (currency_code)
+REFERENCES currencies(currency_code)
+	ON DELETE CASCADE
+;
+--12
+ALTER TABLE countries_rivers
+	
+ADD CONSTRAINT fk_countries_rivers_rivers
+FOREIGN KEY (river_id)
+REFERENCES rivers(id)
+ON UPDATE CASCADE,
+	
+ADD CONSTRAINT fk_countries_rivers_countries
+FOREIGN KEY (country_code)
+REFERENCES countries(country_code)
+ON UPDATE CASCADE
+;
+--13
+CREATE TABLE customers (
+	id SERIAL PRIMARY KEY,
+	customer_name VARCHAR(100)
+);
+
+CREATE TABLE contacts (
+	id SERIAL PRIMARY KEY,
+	contact_name VARCHAR(100),
+	phone VARCHAR(15),
+	email VARCHAR(50),
+	customer_id INT,
+
+	CONSTRAINT fk_contacts_customers
+	FOREIGN KEY (customer_id)
+	REFERENCES customers(id)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE
+);
+
+INSERT INTO customers (customer_name)
+VALUES
+	('BlueBird Inc'),
+	('Dolphin LLC');
+
+INSERT INTO contacts (customer_id, contact_name, phone, email)
+VALUES
+	(1, 'John Doe', '(408)-111-1234', 'john.doe@bluebird.dev'),
+	(1, 'Jane Doe', '(408)-111-1235', 'jane.doe@bluebird.dev'),
+	(2, 'David Wright', '(408)-222-1234', 'david.wright@dolphin.dev')
+;
+
+DELETE FROM customers
+WHERE id = 1;
+--14
+SELECT 
+	mountain_range,
+	peak_name,
+	elevation
+FROM peaks
+JOIN mountains
+ON mountains.id = peaks.mountain_id
+WHERE mountain_range LIKE '%Rila%'
+ORDER BY elevation DESC;
+--15
+
